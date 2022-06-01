8cc17a67f80cc1253240a00a2ddf3aae3c7b2047
yoon <dbsgpfla123@naver.com>
2022-06-01 23:12:48 +0900

    정상, 수정 BR_W_IM_151 FAQ 전체 조회 BR_W_IM_152 FAQ 입력 BR_W_IM_153 FAQ 수정 BR_W_IM_154 FAQ 삭제
    BR_W_IM_154 FAQ 삭제
    수정 : 다이나믹쿼리로 중복삭제 가능하도록 수정함
    
    Test 중 오류메세지 오독해로 Vo부분 getter,setter 추가하였음

--- BR_LIS_Ver1/src/test/java/com/br/lis/TestLisJunit_YHL.java
+++ BR_LIS_Ver1/src/test/java/com/br/lis/TestLisJunit_YHL.java
@@ -1,8 +1,11 @@
 package com.br.lis;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -14,8 +17,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.br.lis.model.board.mapper.ICalendarBoardDao;
 import com.br.lis.model.board.service.ICalendarBoardService;
+import com.br.lis.model.board.service.IFAQBoardService;
 import com.br.lis.model.test.service.ITestService;
 import com.br.lis.vo.CalendarBoardVo;
+import com.br.lis.vo.Notice_FAQBoardVo;
 
 
 
@@ -28,14 +33,93 @@ public class TestLisJunit_YHL {
 	
 	@Autowired
 	private ICalendarBoardService service;
-
+	
+	@Autowired
+	private IFAQBoardService serviceFAQ;
+	
+	
+//	@Test
+	public void viewAllFAQ() {
+		
+		System.out.println("Junit_viewAllFAQ_FAQ 전체 조회");
+		List<Notice_FAQBoardVo> lists = serviceFAQ.viewAllFAQ();
+		
+	}
+	
+//	@Test
+	public void insertFAQ() {
+		System.out.println("Junit_insertFAQ FAQ 입력");
+		String admin_id ="admin001" ;
+		String title = "대출한 책을 가까운 도서관에 반납할 수 있나요?";
+		String content = "불가능 합니다. 죄송하지만 본 도서관은 상호대차 서비스를 제공하지 않고 있습니다.";
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("admin_id", admin_id);
+		map.put("title", title);
+		map.put("content", content);
+		
+		if (serviceFAQ.insertFAQ(map)>0) {
+			System.out.println("새글 입력에 성공하였습니다.");
+			
+		}else {
+			System.out.println("새글 입력에 실패하였습니다.");
+		}
+	}
+		
+//	@Test
+	public void modifyFAQ() {
+		System.out.println("Junit_modifyFAQ FAQ 수정");
+		Notice_FAQBoardVo vo = new Notice_FAQBoardVo();
+		vo.setFaq_seq("1");
+		vo.setTitle("도서대출회원에 가입하려면 어떻게 해야 되나요?");
+		vo.setContent("김포시민(주소가 등본 상 서울시로 되어 있는 사람, 서울시 소재 학교 및 직장을 다니는 사람)이여야 하며 회원가입절차는 다음과 같습니다.\r\n"
+				+ "\r\n"
+				+ "1. 본인인증(도서관 홈페이지 또는 방문)\r\n"
+				+ "- 본인인증방법 : 인쇄된 등본 지참 혹은 홈페이지에 이미지 업로드\r\n"
+				+ "- 도서관 홈페이지에서 이용시 :\r\n"
+				+ "  로그인 화면으로 이동 > 도서대출 회원가입 > 회원가입\r\n"
+				+ "- 도서관 방문하여 이용시 : 직원에게 문의하시면 안내해 드립니다.\r\n"
+				+ "2. 회원가입 서류(확인)\r\n"
+				+ "- 성 인 : 주소가 기재되어 있는 신분증(주소 미기재 시 등본 필요)\r\n"
+				+ "- 재직자 : 김포시 주소가 기재된 재직증명서와 신분증\r\n"
+				+ "- 학 생 : 학생증과 등본\r\n"
+				+ "- 어린이(만14세 미만) : 부모님 신분증과 등본 지참, 부모님과 동행하여 방문\r\n"
+				+ "(만14세 미만의 어린이는 법정대리인의 동의가 필요하므로 부모님 동행 필수)");
+		serviceFAQ.modifyFAQ(vo);
+	}
 	
 	@Test
-	public void test() {
-		List<CalendarBoardVo> lists = service.viewAllCalendar();
-		System.out.println("--------CalendarBoardVo--------------------viewAllCalendar---------------------------------------------------");
-
-		System.out.println(lists);
-	
+	public void deleteFAQ() {
+		System.out.println("JUnit deleteFAQ FAQ 삭제");
+		List<String> list = new ArrayList<String>();
+		
+		list.add("");
+		list.add("");
+		list.add("");
+		
+ 		int cnt = serviceFAQ.deleteFAQ(list);
+ 		
+		if (cnt>0) {
+			System.out.println("글 삭제에 성공하였습니다 삭제한 글의 개수 : "+cnt);
+		}else {
+			System.out.println("글 삭제에 실패하였습니다.");
+		}
 	}
+	
+
+	
+//	@Test //캘린더
+//	public void test() {
+//		List<CalendarBoardVo> lists = service.viewAllCalendar();
+//		System.out.println("--------CalendarBoardVo--------------------viewAllCalendar---------------------------------------------------");
+//
+//		System.out.println(lists);
+//	
+//	}
+	
+	
+	
+	
+	
+	
 }
